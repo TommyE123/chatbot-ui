6371d3a1f62e44953444b02126cf466388703660
Tom Eley <Thomas_Eley@hotmail.com>
2023-03-31 19:05:03 +0100

    stylelint config updates

--- stylelint.config.js
+++ stylelint.config.js
@@ -1,17 +1,7 @@
 module.exports = {
-  plugins: ['stylelint-order'],
-  extends: ['stylelint-config-standard',
-            'stylelint-config-recommended-scss',
-            'stylelint-config-prettier'],
+  plugins: ['stylelint-prettier'],
+  extends: ['stylelint-prettier/recommended', 'stylelint-config-prettier'],
   rules: {
-    'order/order': ['custom-properties', 'declarations'],
-    'order/properties-alphabetical-order': true,
-    'at-rule-no-unknown': null,
-    'scss/at-rule-no-unknown': true,
-    'scss/at-function-named-arguments': null,
-    'scss/at-function-pattern': '^[_a-z]+([_a-z0-9-]+[a-z0-9]+)?$',
-    'function-calc-no-unspaced-operator': null,
-    'declaration-empty-line-before': null,
-    'no-descending-specificity': null,
+    'prettier/prettier': true,
   },
 };

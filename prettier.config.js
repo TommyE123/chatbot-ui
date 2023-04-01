86597c6ee569d7d4d8a1f54bf54893eb0bfada90
Tom Eley <Thomas_Eley@hotmail.com>
2023-04-01 13:45:05 +0100

    overrides

--- prettier.config.js
+++ prettier.config.js
@@ -1,5 +1,13 @@
 module.exports = {
   trailingComma: 'all',
   singleQuote: true,
-  plugins: [require('prettier-plugin-tailwindcss')]
+  plugins: [require('prettier-plugin-tailwindcss')],
+  overrides: [
+    {
+      files: '*.ts',
+      options: {
+        parser: 'typescript',
+      },
+    },
+  ],
 };
